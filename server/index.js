e422860b76ba751fbb03b57ad5162a8bd92725f4
Romit Mohane <69901221+Reckadon@users.noreply.github.com>
2024-01-07 16:32:32 +0530

    made nodemon a dev dependency and serve static files

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const app = express();
+const path = require("path");
+
 const port = 3000;
 
-app.get("/", (req, res) => {
-	res.send("API for Pictionary App");
-});
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.listen(port, () => {
 	console.log(`listening on port ${port}`);
