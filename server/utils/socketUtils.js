f9ac5280d7773f081f3057ff85a5d104298b7378
Romit Mohane <69901221+Reckadon@users.noreply.github.com>
2024-01-18 17:25:47 +0530

    implemented automatic removal of player from room data if player disconnects and also deleting room if no more players are left in the room

--- server/utils/socketUtils.js
+++ server/utils/socketUtils.js
@@ -29,13 +29,16 @@ export const registerEvents = socket => {
 			players: Rooms[Rooms.length - 1].players,
 			roomID: Rooms[Rooms.length - 1].roomID,
 		});
+
+		registerDisconnectListener(socket, uuid, 0);
 	});
 
 	socket.on(SocketEventNames.JoinRoom, (username, roomID, callback) => {
 		if (roomExists(roomID)) {
 			socket.join(roomID);
 			const i = Rooms.findIndex(room => room.roomID === roomID);
-			Rooms[i].addPlayer(new Player(Rooms[i].idForNextPlayer, username));
+			const playerID = Rooms[i].idForNextPlayer;
+			Rooms[i].addPlayer(new Player(playerID, username));
 			console.log(`${username} joined room ${roomID}`);
 			callback({ status: 200 });
 			socket.emit(SocketEventNames.InitialRoomData, {
@@ -45,6 +48,8 @@ export const registerEvents = socket => {
 			socket.broadcast
 				.to(roomID)
 				.emit(SocketEventNames.NewPlayer, Rooms[i].players[Rooms[i].players.length - 1]);
+
+			registerDisconnectListener(socket, roomID, playerID);
 		} else {
 			callback({ status: 404 });
 		}
@@ -56,10 +61,24 @@ export const registerEvents = socket => {
 		console.log(`player with id:${id} left room ${roomID}`);
 		Rooms[i].players = Rooms[i].players.filter(player => player.id !== id);
 		if (Rooms[i].players.length === 0) {
-			Rooms = Rooms.filter(room => room.roomID !== roomID);
+			Rooms.splice(i);
 			console.log(`deleted room ${roomID} as there are no players left`);
 		}
 		callback();
 		socket.broadcast.to(roomID).emit(SocketEventNames.PlayerLeave, id);
+	});
+};
+
+const registerDisconnectListener = (socket, roomID, playerID) => {
+	socket.on("disconnect", () => {
+		console.log("disconnect");
+		const i = Rooms.findIndex(room => room.roomID === roomID);
+		Rooms[i].players = Rooms[i].players.filter(player => player.id !== playerID);
+		if (Rooms[i].players.length === 0) {
+			Rooms.splice(i, 1);
+			console.log(`deleted room ${roomID} as there are no players left`);
+		}
+		console.log(Rooms);
+		socket.broadcast.to(roomID).emit(SocketEventNames.PlayerLeave, playerID);
 	});
 };
